39480296adb35f0d1ce4e61c2588ecca72f9a3fb
alextaylor <alextaylor515@gmail.com>
2022-07-25 09:13:31 -0400

    commit tree 5d1034b3800fa80e86066eb054aaec6bdfc938cf

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,14 @@
 class Movie < ActiveRecord::Base
-
+    def self.create_with_title title
+        self.create_with(title: title)
+    end
+    def self.first_movie
+        self.first
+    end
+    def self.last_movie
+        self.last
+    end
+    def self.first_movie
+        self.first
+    end
 end
